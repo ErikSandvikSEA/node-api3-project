20c1ced328c44332c69a7894353122c0d74c1f80
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-06-03 11:35:22 -0700

    added logger function

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require('express');
 
+const postRouter = require('./posts/postRouter')
+
 const server = express();
+
+//custom middleware
+
+function logger(req, res, next) {
+  const date = new Date().getSeconds()
+  console.log(`|| Method: ${req.method} `, `|| URL: http://localhost:6000${req.url} `, `|| Timestamp: ${date} seconds`)
+  next()
+}
+
+//global middleware
+server.use(logger)
+server.use(express.json())
+server.use('/api/posts', postRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-//custom middleware
-
-function logger(req, res, next) {}
 
 module.exports = server;
